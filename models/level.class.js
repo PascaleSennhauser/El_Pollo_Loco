c09a2df5d276f04240a98cc4f5115fa5299fba42
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-04-07 16:06:18 +0200

    Added level-file for a better view

---
+++ models/level.class.js
@@ -0,0 +1,11 @@
+class Level {
+    enemies;
+    clouds;
+    backgroundOjects;
+
+    constructor(enemies, clouds, backgroundOjects) {
+        this.enemies = enemies;
+        this.clouds = clouds;
+        this.backgroundOjects = backgroundOjects;
+    }
+}
