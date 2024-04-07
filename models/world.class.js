e8e2e56a09d15eadca20dacf960bd6e608ede374
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-04-07 15:42:14 +0200

    Background changing when pepe is walking

--- models/world.class.js
+++ models/world.class.js
@@ -17,6 +17,7 @@ class World {
     canvas;
     ctx;
     keyboard;
+    camera_x = 0;
 
 
     constructor(canvas, keyboard) {
@@ -35,11 +36,12 @@ class World {
     
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
+        this.ctx.translate(this.camera_x, 0);
         this.addObjectsToMap(this.backgroundObjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.clouds);
         this.addObjectsToMap(this.enemies);
+        this.ctx.translate(-this.camera_x, 0);
   
  
 
