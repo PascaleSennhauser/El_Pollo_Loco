0d8683af9f1121c407559dc0db2cd04ab1000dec
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-04-08 17:10:22 +0200

    Added endboss

--- models/character.class.js
+++ models/character.class.js
@@ -43,11 +43,7 @@ class Character extends MovableObject{
 
         setInterval(() => {
             if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-                // Walk animation
-                let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6; 0,, Rest 0
-                let path = this.IMAGES_WALKING[i];
-                this.img = this.imageCache[path];
-                this.currentImage++;
+                this.playAnimation(this.IMAGES_WALKING);
             }
         }, 50);
     }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -28,6 +28,14 @@ class MovableObject {
     }
 
 
+    playAnimation(images) {
+        let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6; 0,, Rest 0
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+    }
+
+
     moveRight() {
         console.log('Moving right')
     }
