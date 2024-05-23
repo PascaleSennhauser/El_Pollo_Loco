6e1938a60218215bc7b6756dff152cb658b591d8
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-05-23 14:51:49 +0200

    Added jumping function and made some refactoring

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -8,6 +8,23 @@ class MovableObject {
     currentImage = 0;
     speed = 0.08;
     otherDirection = false;
+    speedY = 0;
+    acceleration = 2.5;
+
+
+    applyGravity() {
+        setInterval(() => {
+            if(this.isAboveGround() || this.speedY > 0) {
+                this.y -= this.speedY;
+                this.speedY -= this.acceleration;
+            }
+        }, 1000/25);
+    }
+
+
+    isAboveGround() {
+        return this.y < 125;
+    }
 
 
     loadImage(path) {
@@ -37,13 +54,15 @@ class MovableObject {
 
 
     moveRight() {
-        console.log('Moving right')
+        this.x += this.speed;
     }
 
     
     moveLeft() {
-        setInterval(() => {
-            this.x -= this.speed;
-        }, 1000 / 60);
+        this.x -= this.speed;
+    }
+
+    jump() {
+        this.speedY = 30;
     }
 }
