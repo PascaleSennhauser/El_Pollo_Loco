db2b4d3a70ae84b7aa1a6266df88463cddf0f199
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-06-18 11:07:48 +0200

    Added touchEvents to the mobile versions

--- js/game.js
+++ js/game.js
@@ -136,13 +136,44 @@ function getAmountOfCoins() {
 function touchEvents() {
     document.getElementById('btnRight').addEventListener('touchstart', (e) => {
         e.preventDefault();
-        keyboard.LEFT = true;
+        keyboard.RIGHT = true;
     });
 
     document.getElementById('btnRight').addEventListener('touchend', (e) => {
         e.preventDefault();
+        keyboard.RIGHT = false;
+    });
+
+    document.getElementById('btnLeft').addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        keyboard.LEFT = true;
+    });
+
+    document.getElementById('btnLeft').addEventListener('touchend', (e) => {
+        e.preventDefault();
         keyboard.LEFT = false;
     });
+
+    document.getElementById('btnJump').addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        keyboard.UP = true;
+    });
+
+    document.getElementById('btnJump').addEventListener('touchend', (e) => {
+        e.preventDefault();
+        keyboard.UP = false;
+    });
+
+    document.getElementById('btnThrow').addEventListener('touchstart', (e) => {
+        e.preventDefault();
+        keyboard.SPACE = true;
+    });
+
+    document.getElementById('btnThrow').addEventListener('touchend', (e) => {
+        e.preventDefault();
+        keyboard.SPACE = false;
+    });
+
 }
 
 
